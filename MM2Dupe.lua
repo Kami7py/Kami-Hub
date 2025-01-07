d3ff861b3cd509cecf5f114ed74498d3785103ed
Kami7py <kami7.py@gmail.com>
2025-01-07 02:46:51 -0300

    Update MM2Dupe.lua

--- MM2Dupe.lua
+++ MM2Dupe.lua
@@ -1,142 +1,79 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({Name = "kamihub | MM2 GODLY DUPE 🔪 ", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
 
-getgenv().tpPlace = "string"
-getgenv().autotp = true
-getgenv().rb = true
-getgenv().spin = true
-getgenv().bspin = true
 
-OrionLib:MakeNotification({
-	Name = "kamihub | warning",
-	Content = "❗❗ MUST BE OVER LEVEL 15 ❗❗",
-	Image = "rbxassetid://4483345998",
-	Time = 8
+
+local Tab = Window:MakeTab({
+    Name = "Changer",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
 })
 
-OrionLib:MakeNotification({
-	Name = "kamihub | warning",
-	Content = "❗❗ MUST BE A GODLY TO USE MOST OF THE SCRIPT ❗❗",
-	Image = "rbxassetid://4483345998",
-	Time = 8
-})
+boxmodule = require(game:GetService("ReplicatedStorage").Modules.BoxModule)
+poop = getsenv(game:GetService("Players").LocalPlayer.PlayerGui.MainGUI.Inventory.NewItem)._G
+itemdatabase = require(game:GetService("ReplicatedStorage").Database.Sync.Item)
 
-OrionLib:MakeNotification({
-	Name = "kamihub | warning",
-	Content = "❗❗ THIS SCRIPT WILL NOT WORK IF YOU ARENT IN MM2 ❗❗",
-	Image = "rbxassetid://4483345998",
-	Time = 8
-})
-
-function Teleport(teleportPlace)
-    local player = game.Players.LocalPlayer
-    player.Character.HumanoidRootPart.CFrame = teleportPlace
+function getrandombox()
+    local boxes = require(game:GetService("ReplicatedStorage").Database.Sync.MysteryBox)
+    if not boxes or next(boxes) == nil then
+        return nil
+    end
+    local keys = {}
+    for k,_ in pairs(boxes) do
+        table.insert(keys, k)
+    end
+    local randomIndex = math.random(1, #keys)
+    return keys[randomIndex]
 end
 
-function autotp()
-     while getgenv().autotp == true do
-	 Teleport(game:GetService("Workspace").Wins.SteampunkStage.CFrame)
-	 wait(0.1)
-	end
+function opencrate(ITEM_NAME)
+    print(ITEM_NAME)
+    boxmodule.OpenBox(getrandombox(), ITEM_NAME)
+    poop.NewItem(ITEM_NAME, nil, nil, 'Weapons', 1)
 end
 
-function rb()
-	 while getgenv().rb == true do
-      game:GetService("ReplicatedStorage").RebirthEvent:FireServer()
-	wait(0.1)
-   end
+function getrawnamebyrealname(realname)
+    for i,v in pairs(itemdatabase) do
+        if realname == i then
+            return i
+        end
+    end
 end
 
-function spin()
-	 while getgenv().spin == true do
-     game:GetService("ReplicatedStorage").SpinRemote:InvokeServer()
-      wait(0.1)
-   end
+function gettable(uu)
+    nikita_gay = {}
+    for i,v in pairs(itemdatabase) do
+        if string.find(i:lower(), uu:lower()) then
+            table.insert(nikita_gay, i)
+        end
+    end
+    return nikita_gay
 end
 
-function bspin()
-	 while getgenv().bspin == true do
-	 game:GetService("ReplicatedStorage").SpinnerUpdate:FireServer("BuySpin")
-     wait(0.1)
-   end
-end
-
-local TpTab = Window:MakeTab({
-	Name = "Dupe",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
+nikita = Tab:AddDropdown({
+    Name = "Pick Item",
+    Default = "please filter UR pets",
+    Options = {"please filter UR pets"},
+    Callback = function(Value)
+        getgenv().newValue = getrawnamebyrealname(Value)
+    end    
 })
 
-local otherTab = Window:MakeTab({
-	Name = "Other",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
+Tab:AddTextbox({
+    Name = "Filter Database",
+    Default = "",
+    TextDisappear = true,
+    Callback = function(Value)
+        eee = gettable(Value)
+        wait(1)
+        nikita:Refresh(eee, true)
+    end	  
 })
 
-TpTab:AddParagraph("Note","❗Must follow all the steps to work❗")
-
-TpTab:AddParagraph("STEP 1","break MM2 Anti-Cheat to prevent kick")
-
-TpTab:AddButton({
-	Name = "Bypass Anti-Cheat",
-	Callback = function()
-      		game.StarterGui:SetCore("SendNotification", {Title="kamihub"; Text="Bypass ANTI-CHEAT Activated ✔️"; Duration=5;})
-  	end    
-})
-
-TpTab:AddParagraph("STEP 2","write the name of the desired knife, press enter to conform and reset the character")
-
-TpTab:AddTextbox({
-	Name = "🔪 Knife/Gun 🔫",
-	Default = "WRITE HERE",
-	TextDisappear = true,
-	Callback = function(Value)
-		print(Value)
-	end	  
-})
-
-TpTab:AddParagraph("STEP 3","check Dupe Godly only if you had done all above ")
-
-TpTab:AddToggle({
-	Name = "Dupe Godly",
-	Default = false,
-	Callback = function(Value)
-		getgenv().autotp = Value
-        autotp()
-	end    
-})
-
-otherTab:AddSlider({
-	Name = "WalkSpeed",
-	Min = 0,
-	Max = 100,
-	Default = 16,
-	Color = Color3.fromRGB(255,255,255),
-	Increment = 1,
-	ValueName = "Speed",
-	Callback = function(Value)
-		game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = (Value)
-   end
-})
-
-otherTab:AddSlider({
-	Name = "JumpPower",
-	Min = 0,
-	Max = 100,
-	Default = 16,
-	Color = Color3.fromRGB(255,255,255),
-	Increment = 1,
-	ValueName = "Jump",
-	Callback = function(Value)
-		game.Players.LocalPlayer.Character.Humanoid.JumpPower = (Value)
-   end
-})
-
-TpTab:AddDropdown({
-	Name = "Select",
-	Default = "Success Rate 100%",
-	Options = {"Success Rate 100%", "Success Rate 80%"},
-	Callback = function(Value)
-	    getgenv().tpPlace = Value  
-	end    
+Tab:AddButton({
+    Name = "Open Crate",
+    Icon = "rbxassetid://4483345998",
+    Callback = function()
+        opencrate(getgenv().newValue)
+    end
 })
